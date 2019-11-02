3f24f6039a4b104a0bbb76a362209f4ac7febe0c
ShenZitong <1043927617@qq.com>
2019-11-02 11:57:20 +0800

    'cs'

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
 import router from './router'
-
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 导入不应用会报错
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
